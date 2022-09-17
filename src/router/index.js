e0e9e20c52b3a871372c48a543c144e90c7014aa
GanbadeiKnight <1609545937@qq.com>
2022-09-17 21:46:21 +0800

    完成

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -27,7 +28,8 @@ const router = new VueRouter({
 		  {path:'/params',component: Params},
 		  {path:'/goods',component:GoodsList},
 		  {path:'/goods/add',component:Add},
-		  {path:'/orders',component:Order}
+		  {path:'/orders',component:Order},
+		  {path:'/reports',component:Report}
 	  ]}
   ]
 })
